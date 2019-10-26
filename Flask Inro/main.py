f9a693d1e7d38ccd4b69537f6d15751a7fc24b84
Ivan Sukhanov <46958261+IvanSukhanov4@users.noreply.github.com>
2019-10-26 22:29:31 +0300

    Add files via upload

---
+++ Flask Inro/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+
+@app.route("/vegetables")
+def get_vegetables():
+    vegetables = ['beans', 'carrot', 'beetroot', 'cucumber']
+    return render_template("vegetables.html", vegetables=vegetables)
+
+
+@app.route("/fruits")
+def get_fruits():
+    fruits = ['melon', 'apple', 'strawberry', 'grape']
+    return render_template("fruits.html", fruits=fruits)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
