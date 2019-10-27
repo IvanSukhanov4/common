33b15b28caff4059488d700cdc537661706662c9
Ivan Sukhanov <46958261+IvanSukhanov4@users.noreply.github.com>
2019-10-27 16:51:23 +0200

    Add files via upload
    Block info for all cases

--- Flask Inro/main.py
+++ Flask Inro/main.py
@@ -12,13 +12,13 @@ def home_page():
 @app.route("/vegetables")
 def get_vegetables():
     vegetables = ['beans', 'carrot', 'beetroot', 'cucumber']
-    return render_template("home.html", vegetables=vegetables)
+    return render_template("vegetables.html", vegetables=vegetables)
 
 
 @app.route("/fruits")
 def get_fruits():
     fruits = ['melon', 'apple', 'strawberry', 'grape']
-    return render_template("home.html", fruits=fruits)
+    return render_template("fruits.html", fruits=fruits)
 
 
 if __name__ == '__main__':
